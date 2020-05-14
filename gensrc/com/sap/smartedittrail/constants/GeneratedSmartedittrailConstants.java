e73fc55bdde026afca27a254add6957063c76729
yaelmarzak <yasserelmarzak@gmail.com>
2020-05-14 17:54:07 +0000

    Adding A New Page To SmartEdit

---
+++ gensrc/com/sap/smartedittrail/constants/GeneratedSmartedittrailConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 14 mai 2020 à 17:38:08                      ---
+ * ----------------------------------------------------------------
+ */
+package com.sap.smartedittrail.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast"})
+public class GeneratedSmartedittrailConstants
+{
+	public static final String EXTENSIONNAME = "smartedittrail";
+	
+	protected GeneratedSmartedittrailConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
